f4272077a58caf3d28c4e7f7efd314e9d9664468
achyuthparth <achyuthaa@outlook.com>
2024-04-23 21:48:25 -0400

    saving things from alex

--- new_shape_generator.py
+++ new_shape_generator.py
@@ -2,49 +2,49 @@ import max_heap
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
-def generator(from_file, to_file):
+def generator(from_file):
     # getting test file
-    al_shape = gpd.read_file(from_file)
+    state_shape = gpd.read_file(from_file)
 
     # creating geo data frame
-    al_prez = gpd.GeoDataFrame()
+    state_prez = gpd.GeoDataFrame()
 
     # create subset with only presidents and geometry
-    al_prez = al_prez.set_geometry(al_shape["geometry"])
+    state_prez = state_prez.set_geometry(state_shape["geometry"])
     add_columns = []
-    for col in al_shape:
+    for col in state_shape:
         if col[3:6] == "PRE":
             add_columns.append(col)
 
     # adding filtered columns with presidents
     for col in add_columns:
-        al_prez[col] = al_shape[col]
+        state_prez[col] = state_shape[col]
 
     # finding the winners by row and saving as a separate column
-    numeric_columns = al_prez.select_dtypes(include='number')  # Select only numeric columns
+    numeric_columns = state_prez.select_dtypes(include='number')  # Select only numeric columns
     max_indices = numeric_columns.idxmax(axis=1)  # Get the index of max value for each row
-    al_prez["winners"] = max_indices
+    state_prez["winners"] = max_indices
 
     # using heap to extract the maximum from each row
     win_count = []
-    for index, row in al_prez.iterrows():
-        numeric_values = al_prez.loc[index, numeric_columns.columns].tolist() # get the row of the numbers
+    for index, row in state_prez.iterrows():
+        numeric_values = state_prez.loc[index, numeric_columns.columns].tolist() # get the row of the numbers
         heap = max_heap.MaxHeap(20)
         for item in numeric_values:
             heap.insert(item)
         max_val = heap.extractMax()
         win_count.append(max_val)
 
-    al_prez["win_count"] = win_count
+    state_prez["win_count"] = win_count
 
     # setting the color depending on the winner of that particular row
-    for i, winner in enumerate(al_prez["winners"]):
+    for i, winner in enumerate(state_prez["winners"]):
         if winner[6] == "R": color = "red"
         elif winner[6] == "D": color = "blue"
         elif winner[6] == "G": color = "green"
         elif winner[6] == "L": color = "yellow"
         else: color = "grey"
         
-        al_prez.loc[i, "max_color"] = color
-
-    gpd.GeoDataFrame.to_file(to_file)
+        state_prez.loc[i, "max_color"] = color
+    
+    return state_prez
